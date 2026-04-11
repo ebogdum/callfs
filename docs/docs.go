f83fdcbdcc1cf47ea1b7a68cb57a36b512e9672f
Bodo <admin.nxpoint@gmail.com>
2026-04-11 07:06:52 +0200

    release: bump version to v1.4.0

--- docs/docs.go
+++ docs/docs.go
@@ -696,7 +696,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.3.0",
+	Version:          "1.4.0",
 	Host:             "localhost:8443",
 	BasePath:         "/v1",
 	Schemes:          []string{},
