edd0474eb536ec59205ec9a5d8b5e4ece18ab433
Bodo <admin.nxpoint@gmail.com>
2026-04-11 07:06:52 +0200

    release: bump version to v1.4.0

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 //	@title		CallFS API
-//	@version	1.3.0
+//	@version	1.4.0
 //	@description	CallFS is an ultra-lightweight, high-performance REST API filesystem that provides precise Linux filesystem semantics over various backends.
 //	@termsOfService	http://swagger.io/terms/
 
